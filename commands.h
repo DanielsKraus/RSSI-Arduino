3c6e37fe7362d71f5dd5b846f740f2775efe11bf
GHOST <48442414+DanielsKraus@users.noreply.github.com>
2019-09-07 00:40:29 -0400

    Create commands.h

---
+++ commands.h
@@ -0,0 +1,30 @@
+#ifndef commands_h
+#define commands_h
+
+class commands()
+{
+  private:
+    String prof;
+    String role;
+    String pass;
+    String addr;
+    String scan;
+    String type;
+    
+  public:
+    void setSpp_profile(String);
+    void setRole(String);
+    void setPswd(String);
+    void setAddress(String);
+    void setSeek(String);
+    void setRecent(String); 
+
+    String getSpp_profile();
+    String getRole();
+    String getPswd();
+    String getAddress();
+    String getSeek();
+    String getRecent();
+};
+
+#endif
